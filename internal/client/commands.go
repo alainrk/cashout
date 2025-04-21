0a989245b9290641ec4ff8f8af70f7cfe791f2ad
Alain <alain.devcs@gmail.com>
2025-04-21 07:44:22 +0200

    refactoring to use inline keyboard

--- internal/client/commands.go
+++ internal/client/commands.go
@@ -11,77 +11,71 @@ import (
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
-func (c *Client) Test(b *gotgbot.Bot, ctx *ext.Context) error {
-	b.SendMessage(ctx.EffectiveSender.ChatId, "test inline", &gotgbot.SendMessageOpts{
-		ReplyMarkup: gotgbot.InlineKeyboardMarkup{
-			InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
-				{{Text: "Income", CallbackData: "test.income"}},
-				{{Text: "Expense", CallbackData: "test.expense"}},
-			},
-		},
-	})
-
-	return nil
-}
-
-func (c *Client) TestInlineCallback(b *gotgbot.Bot, ctx *ext.Context) error {
-	fmt.Println("inline called with query", ctx.CallbackQuery)
-	query := ctx.CallbackQuery
-	// user := ctx.EffectiveSender.User
-	msg := query.Message
-
-	action := strings.Split(query.Data, ".")[1]
-
-	switch action {
-	case "income":
-		_, _, err := msg.EditText(b, "This is an income", nil)
-		if err != nil {
-			fmt.Printf("failed to set user data: %v", err)
-			return err
-		}
-	case "expense":
-		_, _, err := msg.EditText(b, "This is an expense", nil)
-		if err != nil {
-			fmt.Printf("failed to set user data: %v", err)
-			return err
-		}
-	}
-
-	msg.EditReplyMarkup(b, &gotgbot.EditMessageReplyMarkupOpts{
-		ReplyMarkup: gotgbot.InlineKeyboardMarkup{
-			InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
-				{{Text: "Income", CallbackData: "test.income"}},
-				{{Text: "Expense", CallbackData: "test.expense"}},
-			},
-		},
-	})
-
-	return ext.EndGroups
-}
+// func (c *Client) Test(b *gotgbot.Bot, ctx *ext.Context) error {
+// 	b.SendMessage(ctx.EffectiveSender.ChatId, "test inline", &gotgbot.SendMessageOpts{
+// 		ReplyMarkup: gotgbot.InlineKeyboardMarkup{
+// 			InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+// 				{{Text: "Income", CallbackData: "test.income"}},
+// 				{{Text: "Expense", CallbackData: "test.expense"}},
+// 			},
+// 		},
+// 	})
+//
+// 	return nil
+// }
+//
+// func (c *Client) TestInlineCallback(b *gotgbot.Bot, ctx *ext.Context) error {
+// 	fmt.Println("inline called with query", ctx.CallbackQuery)
+// 	query := ctx.CallbackQuery
+// 	// user := ctx.EffectiveSender.User
+// 	msg := query.Message
+//
+// 	action := strings.Split(query.Data, ".")[1]
+//
+// 	switch action {
+// 	case "income":
+// 		_, _, err := msg.EditText(b, "This is an income", nil)
+// 		if err != nil {
+// 			fmt.Printf("failed to set user data: %v", err)
+// 			return err
+// 		}
+// 	case "expense":
+// 		_, _, err := msg.EditText(b, "This is an expense", nil)
+// 		if err != nil {
+// 			fmt.Printf("failed to set user data: %v", err)
+// 			return err
+// 		}
+// 	}
+//
+// 	msg.EditReplyMarkup(b, &gotgbot.EditMessageReplyMarkupOpts{
+// 		ReplyMarkup: gotgbot.InlineKeyboardMarkup{
+// 			InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+// 				{{Text: "Income", CallbackData: "test.income"}},
+// 				{{Text: "Expense", CallbackData: "test.expense"}},
+// 			},
+// 		},
+// 	})
+//
+// 	return ext.EndGroups
+// }
 
 // Start introduces the bot.
 func (c *Client) Start(b *gotgbot.Bot, ctx *ext.Context) error {
-	user, err := c.authAndGetUser(ctx)
+	_, u := c.getUserFromContext(ctx)
+	user, err := c.authAndGetUser(u)
 	if err != nil {
 		return err
 	}
 
 	msg := fmt.Sprintf("Welcome to HappyPoor, %s!", user.Name)
-	c.SendTransactionKeyboard(b, ctx, msg)
+	c.SendAddTransactionKeyboard(b, ctx, msg)
 
 	return nil
 }
 
-func (c *Client) AddIncomeIntent(b *gotgbot.Bot, ctx *ext.Context) error {
-	return c.addTransactionIntent(b, ctx, model.TypeIncome)
-}
-
-func (c *Client) AddExpenseIntent(b *gotgbot.Bot, ctx *ext.Context) error {
-	return c.addTransactionIntent(b, ctx, model.TypeExpense)
-}
-
-func (c *Client) addTransactionIntent(b *gotgbot.Bot, ctx *ext.Context, transactionType model.TransactionType) error {
-	user, err := c.authAndGetUser(ctx)
+func (c *Client) AddTransactionIntent(b *gotgbot.Bot, ctx *ext.Context) error {
+	_, u := c.getUserFromContext(ctx)
+	user, err := c.authAndGetUser(u)
 	if err != nil {
 		return err
 	}
@@ -90,39 +84,48 @@ func (c *Client) addTransactionIntent(b *gotgbot.Bot, ctx *ext.Context, transact
 		return c.Start(b, ctx)
 	}
 
-	user.Session.LastCommand = model.CommandAddExpenseIntent
-	if transactionType == model.TypeIncome {
-		user.Session.LastCommand = model.CommandAddIncomeIntent
+	query := ctx.CallbackQuery
+	msg := query.Message
+
+	action := strings.Split(query.Data, ".")[2]
+
+	switch action {
+	case "income":
+		user.Session.State = model.StateInsertingIncome
+	case "expense":
+		user.Session.State = model.StateInsertingExpense
+	default:
+		c.SendAddTransactionKeyboard(b, ctx, "Invalid action, add a transaction.")
+		return nil
 	}
-	user.Session.State = model.StateWaiting
-	user.Session.LastMessage = ctx.Message.Text
+
+	user.Session.LastMessage = action
 
 	err = c.Repositories.Users.Update(&user)
 	if err != nil {
 		return fmt.Errorf("failed to set user data: %w", err)
 	}
 
-	ctx.EffectiveMessage.Reply(b, "Sure, just tell me category, amount and decription.", &gotgbot.SendMessageOpts{
-		ParseMode: "HTML",
-		ReplyMarkup: gotgbot.ReplyKeyboardMarkup{
-			Keyboard: [][]gotgbot.KeyboardButton{
-				{
-					{
-						Text: "Cancel",
-					},
-				},
+	_, _, err = msg.EditText(b, fmt.Sprintf("Sure, to add a new %s, just tell me category, amount and decription.", action), nil)
+	if err != nil {
+		fmt.Printf("failed to set user data: %v", err)
+		return err
+	}
+
+	msg.EditReplyMarkup(b, &gotgbot.EditMessageReplyMarkupOpts{
+		ReplyMarkup: gotgbot.InlineKeyboardMarkup{
+			InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+				{{Text: "Cancel", CallbackData: "cancel"}},
 			},
-			IsPersistent:    false,
-			OneTimeKeyboard: true,
-			ResizeKeyboard:  true,
 		},
 	})
 
-	return nil
+	return ext.ContinueGroups
 }
 
 func (c *Client) AddTransaction(b *gotgbot.Bot, ctx *ext.Context) error {
-	user, err := c.authAndGetUser(ctx)
+	_, u := c.getUserFromContext(ctx)
+	user, err := c.authAndGetUser(u)
 	if err != nil {
 		return err
 	}
@@ -133,14 +136,13 @@ func (c *Client) AddTransaction(b *gotgbot.Bot, ctx *ext.Context) error {
 
 	var transactionType model.TransactionType
 
-	switch user.Session.LastCommand {
-	case model.CommandAddIncomeIntent:
+	switch user.Session.State {
+	case model.StateInsertingIncome:
 		transactionType = model.TypeIncome
-	case model.CommandAddExpenseIntent:
+	case model.StateInsertingExpense:
 		transactionType = model.TypeExpense
 	default:
-		// answer the user that they should chose a valid command first and send the keyboard
-		c.SendTransactionKeyboard(b, ctx, "Add a transaction")
+		c.SendAddTransactionKeyboard(b, ctx, "Invalid action, add a transaction.")
 		return nil
 	}
 
@@ -153,9 +155,16 @@ func (c *Client) AddTransaction(b *gotgbot.Bot, ctx *ext.Context) error {
 		return err
 	}
 
+	if transaction.Amount == 0 {
+		msg := fmt.Sprintf("I'm sorry, I can't understand your transaction '%s', %s!", user.Session.LastMessage, user.Name)
+		ctx.EffectiveMessage.Reply(b, msg, &gotgbot.SendMessageOpts{
+			ParseMode: "HTML",
+		})
+		return nil
+	}
+
 	// Store the transaction in the session
-	user.Session.State = model.StateWaiting
-	user.Session.LastCommand = model.CommandAddTransaction
+	user.Session.State = model.StateNormal
 	user.Session.LastMessage = ctx.Message.Text
 	s, err := json.Marshal(transaction)
 	user.Session.Body = string(s)
@@ -169,14 +178,34 @@ func (c *Client) AddTransaction(b *gotgbot.Bot, ctx *ext.Context) error {
 	}
 
 	msg := fmt.Sprintf("%s (€ %.2f), %s. Confirm?", transaction.Category, transaction.Amount, transaction.Description)
-	c.SendConfirmKeyboard(b, ctx, msg, []string{"Edit"})
+	_, err = b.SendMessage(ctx.EffectiveSender.ChatId, msg, &gotgbot.SendMessageOpts{
+		ReplyMarkup: gotgbot.InlineKeyboardMarkup{
+			InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+				{
+					{
+						Text:         "Confirm",
+						CallbackData: "transactions.confirm",
+					},
+					{
+						Text:         "Cancel",
+						CallbackData: "transactions.cancel",
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		fmt.Printf("failed to send confirm message: %v\n", err)
+		return err
+	}
 
 	return nil
 }
 
 // Edit edits the transaction previously inserted, basically returns to the same add Income/Expense state, cleaning up the session.
 func (c *Client) AmendTransaction(b *gotgbot.Bot, ctx *ext.Context) error {
-	user, err := c.authAndGetUser(ctx)
+	_, u := c.getUserFromContext(ctx)
+	user, err := c.authAndGetUser(u)
 	if err != nil {
 		return err
 	}
@@ -187,13 +216,12 @@ func (c *Client) AmendTransaction(b *gotgbot.Bot, ctx *ext.Context) error {
 		return fmt.Errorf("failed to extract transaction from the session: %w", err)
 	}
 
-	user.Session.State = model.StateEdit
 	user.Session.LastMessage = ctx.Message.Text
 
 	// Trick to put the session state back to the correct add transaction
-	user.Session.LastCommand = model.CommandAddExpenseIntent
+	user.Session.State = model.StateInsertingExpense
 	if transaction.Type == model.TypeIncome {
-		user.Session.LastCommand = model.CommandAddIncomeIntent
+		user.Session.State = model.StateInsertingIncome
 	}
 	// Clean up the previously inserted session
 	user.Session.Body = ""
@@ -224,7 +252,8 @@ func (c *Client) AmendTransaction(b *gotgbot.Bot, ctx *ext.Context) error {
 
 // Confirm confirms the previous action after the user been prompted.
 func (c *Client) Confirm(b *gotgbot.Bot, ctx *ext.Context) error {
-	user, err := c.authAndGetUser(ctx)
+	_, u := c.getUserFromContext(ctx)
+	user, err := c.authAndGetUser(u)
 	if err != nil {
 		return err
 	}
@@ -236,8 +265,7 @@ func (c *Client) Confirm(b *gotgbot.Bot, ctx *ext.Context) error {
 	}
 
 	user.Session.State = model.StateNormal
-	user.Session.LastCommand = model.CommandConfirm
-	user.Session.LastMessage = ctx.Message.Text
+	user.Session.LastMessage = "confirm"
 	user.Session.Body = ""
 
 	err = c.Repositories.Users.Update(&user)
@@ -248,43 +276,73 @@ func (c *Client) Confirm(b *gotgbot.Bot, ctx *ext.Context) error {
 	transaction.TgID = user.TgID
 	transaction.Currency = model.CurrencyEUR
 
-	c.Repositories.Transactions.Add(transaction)
+	err = c.Repositories.Transactions.Add(transaction)
+	if err != nil {
+		// TODO: Handle send failure message to the user
+		fmt.Printf("failed to add transaction: %v\n", err)
+		return fmt.Errorf("failed to add transaction: %w", err)
+	}
 
-	c.SendTransactionKeyboard(b, ctx, "Your transaction has been saved!")
+	// Remove the keyboard from the previous message
+	query := ctx.CallbackQuery
+	msg := query.Message
+	msg.EditReplyMarkup(b, &gotgbot.EditMessageReplyMarkupOpts{
+		ReplyMarkup: gotgbot.InlineKeyboardMarkup{
+			InlineKeyboard: [][]gotgbot.InlineKeyboardButton{},
+		},
+	})
+
+	emoji := "💰"
+	if transaction.Type == model.TypeExpense {
+		emoji = "💸"
+	}
+	c.SendAddTransactionKeyboard(b, ctx, fmt.Sprintf("%s Your transaction has been saved!", emoji))
 
 	return nil
 }
 
 // Cancel returns to normal state.
 func (c *Client) Cancel(b *gotgbot.Bot, ctx *ext.Context) error {
-	user, err := c.authAndGetUser(ctx)
+	_, u := c.getUserFromContext(ctx)
+	user, err := c.authAndGetUser(u)
 	if err != nil {
 		return err
 	}
 
+	query := ctx.CallbackQuery
+	msg := query.Message
+
 	user.Session.State = model.StateNormal
-	user.Session.LastCommand = model.CommandCancel
-	user.Session.LastMessage = ctx.Message.Text
+	user.Session.LastMessage = "cancel"
 
 	err = c.Repositories.Users.Update(&user)
 	if err != nil {
 		return fmt.Errorf("failed to set user data: %w", err)
 	}
 
-	c.SendTransactionKeyboard(b, ctx, "Add a transaction")
+	msg.EditReplyMarkup(b, &gotgbot.EditMessageReplyMarkupOpts{
+		ReplyMarkup: gotgbot.InlineKeyboardMarkup{},
+		// ReplyMarkup: gotgbot.InlineKeyboardMarkup{
+		// 	InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+		// 		{{Text: "Cancel", CallbackData: "cancel"}},
+		// 	},
+		// },
+	})
 
-	return nil
+	c.SendAddTransactionKeyboard(b, ctx, "Add a transaction")
+
+	return ext.EndGroups
 }
 
 // MonthRecap returns to the user the breakdown and the total for the expenses and income of the current month.
 func (c *Client) MonthRecap(b *gotgbot.Bot, ctx *ext.Context) error {
-	user, err := c.authAndGetUser(ctx)
+	_, u := c.getUserFromContext(ctx)
+	user, err := c.authAndGetUser(u)
 	if err != nil {
 		return err
 	}
 
 	user.Session.State = model.StateNormal
-	user.Session.LastCommand = model.CommandMonthRecap
 	user.Session.LastMessage = ctx.Message.Text
 
 	err = c.Repositories.Users.Update(&user)
@@ -332,13 +390,13 @@ func (c *Client) MonthRecap(b *gotgbot.Bot, ctx *ext.Context) error {
 }
 
 func (c *Client) YearRecap(b *gotgbot.Bot, ctx *ext.Context) error {
-	user, err := c.authAndGetUser(ctx)
+	_, u := c.getUserFromContext(ctx)
+	user, err := c.authAndGetUser(u)
 	if err != nil {
 		return err
 	}
 
 	user.Session.State = model.StateNormal
-	user.Session.LastCommand = model.CommandMonthRecap
 	user.Session.LastMessage = ctx.Message.Text
 
 	err = c.Repositories.Users.Update(&user)
