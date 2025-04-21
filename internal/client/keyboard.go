77ed0c59d48b70c564347383bc6b8f379511ed95
Alain <alain.devcs@gmail.com>
2025-04-21 07:44:28 +0200

    refactoring to use inline keyboard

--- internal/client/keyboard.go
+++ internal/client/keyboard.go
@@ -5,45 +5,13 @@ import (
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
-func (c *Client) SendTransactionKeyboard(b *gotgbot.Bot, ctx *ext.Context, text string) (*gotgbot.Message, error) {
+func (c *Client) SendAddTransactionKeyboard(b *gotgbot.Bot, ctx *ext.Context, text string) (*gotgbot.Message, error) {
 	return b.SendMessage(ctx.EffectiveSender.ChatId, text, &gotgbot.SendMessageOpts{
-		ReplyMarkup: gotgbot.ReplyKeyboardMarkup{
-			Keyboard: [][]gotgbot.KeyboardButton{
-				{
-					{
-						Text: "Add Income",
-					},
-					{
-						Text: "Add Expense",
-					},
-				},
+		ReplyMarkup: gotgbot.InlineKeyboardMarkup{
+			InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+				{{Text: "💰 Add a new income", CallbackData: "transactions.new.income"}},
+				{{Text: "💸 Add a new expense", CallbackData: "transactions.new.expense"}},
 			},
-			IsPersistent:   true,
-			ResizeKeyboard: true,
-		},
-	})
-}
-
-func (c *Client) SendConfirmKeyboard(b *gotgbot.Bot, ctx *ext.Context, text string, additionalButtons []string) (*gotgbot.Message, error) {
-	keyboard := [][]gotgbot.KeyboardButton{
-		{
-			{
-				Text: "Confirm",
-			},
-			{
-				Text: "Cancel",
-			},
-		},
-	}
-	for b := range additionalButtons {
-		keyboard[0] = append(keyboard[0], gotgbot.KeyboardButton{Text: additionalButtons[b]})
-	}
-	return ctx.EffectiveMessage.Reply(b, text, &gotgbot.SendMessageOpts{
-		ReplyMarkup: gotgbot.ReplyKeyboardMarkup{
-			Keyboard:        keyboard,
-			IsPersistent:    false,
-			ResizeKeyboard:  true,
-			OneTimeKeyboard: true,
 		},
 	})
 }
