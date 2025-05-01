5294fd69c2c9ad5c623ba21668b8050b5699becd
Alain <alain.devcs@gmail.com>
2025-05-01 06:20:26 +0200

    add db transactions repo

--- internal/repository/transactions.go
+++ internal/repository/transactions.go
@@ -14,6 +14,18 @@ func (r *Transactions) Add(transaction model.Transaction) error {
 	return r.DB.CreateTransaction(&transaction)
 }
 
+func (r *Transactions) GetByID(id int64) (model.Transaction, error) {
+	transaction, err := r.DB.GetTransactionByID(id)
+	if err != nil {
+		return model.Transaction{}, err
+	}
+	return *transaction, nil
+}
+
+func (r *Transactions) Delete(id int64, tgID int64) error {
+	return r.DB.DeleteTransactionByID(id, tgID)
+}
+
 func (r *Transactions) GetMonthlyTotalsCurrentYear(tgID int64) (map[int]map[model.TransactionType]float64, error) {
 	year := time.Now().Year()
 	return r.DB.GetMonthlyTotalsInYear(tgID, year)
@@ -21,6 +33,10 @@ func (r *Transactions) GetMonthlyTotalsCurrentYear(tgID int64) (map[int]map[mode
 
 func (r *Transactions) GetUserTransactionsByMonthPaginated(tgID int64, year, month, offset, limit int) ([]model.Transaction, int64, error) {
 	return r.DB.GetUserTransactionsByMonthPaginated(tgID, year, month, offset, limit)
+}
+
+func (r *Transactions) GetUserTransactionsByDateRangePaginated(tgID int64, startDate, endDate time.Time, offset, limit int) ([]model.Transaction, int64, error) {
+	return r.DB.GetUserTransactionsByDateRangePaginated(tgID, startDate, endDate, offset, limit)
 }
 
 // GetMonthCategorizedTotals returns the transaction totals for each category for a specific month
