5ee5e836155cb8e6d444e87d87f4dfe25ee3e2c4
Alain <alain.devcs@gmail.com>
2025-05-03 13:32:33 +0200

    top-level missing intent handling

--- internal/client/router.go
+++ internal/client/router.go
@@ -38,5 +38,8 @@ func (c *Client) FreeTextRouter(b *gotgbot.Bot, ctx *ext.Context) error {
 		return c.editTransactionCategory(b, ctx, user)
 	}
 
+	c.CleanupKeyboard(b, ctx)
+	c.SendHomeKeyboard(b, ctx, "Sorry I don't understand, what can I do for you?\n\n/delete - Delete a transaction\n/list - List your transactions\n/month Month Recap\n/year Year Recap")
+
 	return fmt.Errorf("invalid top-level state")
 }
