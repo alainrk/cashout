10d0cced3d59e064c71d87fb40b592631abac3ab
Alain <alain.devcs@gmail.com>
2025-05-03 14:30:44 +0200

    abstract SendMessage to support inline and toplevel home functions

--- internal/client/start.go
+++ internal/client/start.go
@@ -16,7 +16,7 @@ func (c *Client) Start(b *gotgbot.Bot, ctx *ext.Context) error {
 	}
 
 	msg := fmt.Sprintf("Welcome to Cashout, %s!\nWhat can I do for you?\n\n/delete - Delete a transaction\n/list - List your transactions\n/month Month Recap\n/year Year Recap", user.Name)
-	c.SendAddTransactionKeyboard(b, ctx, msg)
+	c.SendHomeKeyboard(b, ctx, msg)
 
 	return nil
 }
