35caf5eca1c677346af832085de6875f07acca4b
Alain <alain.devcs@gmail.com>
2025-05-23 04:40:22 +0200

    UX: at top level the bot get the text as a new expense (most common case)

--- internal/client/router.go
+++ internal/client/router.go
@@ -66,6 +66,19 @@ func (c *Client) FreeTextRouter(b *gotgbot.Bot, ctx *ext.Context) error {
 
 	// End of top-level edit transaction
 
+	// Default behavior: start expense transaction flow for any unhandled text.
+	// Heuristic: there must be at least a digit in text.
+	if strings.ContainsAny(ctx.Message.Text, "0123456789") {
+		user.Session.State = model.StateInsertingExpense
+		err = c.Repositories.Users.Update(&user)
+		if err != nil {
+			return fmt.Errorf("failed to set user data: %w", err)
+		}
+
+		// Process as expense transaction
+		return c.addTransaction(b, ctx, user)
+	}
+
 	c.CleanupKeyboard(b, ctx)
 	c.SendHomeKeyboard(b, ctx, "Sorry I don't understand, what can I do for you?\n\n/edit - Edit a transaction\n/delete - Delete a transaction\n/list - List your transactions\n/month Month Recap\n/year Year Recap")
 
