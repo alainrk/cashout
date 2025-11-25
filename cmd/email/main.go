7979703463001dad447011b0aa37d2f0ff240928
Alain <alain.devcs@gmail.com>
2025-11-25 06:21:06 +0100

    add email support basics

---
+++ cmd/email/main.go
@@ -0,0 +1,49 @@
+// Package main just to test Brevo API client
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	brevo "github.com/getbrevo/brevo-go/lib"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading %s file", ".env")
+	}
+
+	var ctx context.Context
+	cfg := brevo.NewConfiguration()
+	cfg.AddDefaultHeader("api-key", os.Getenv("BREVO_API_KEY"))
+	cfg.AddDefaultHeader("partner-key", os.Getenv("BREVO_API_KEY"))
+
+	br := brevo.NewAPIClient(cfg)
+
+	result, resp, err := br.AccountApi.GetAccount(ctx)
+	if err != nil {
+		fmt.Println("Error when calling AccountApi->get_account: ", err.Error())
+		return
+	}
+
+	fmt.Println("GetAccount Object:", result, " GetAccount Response: ", resp)
+
+	br.TransactionalEmailsApi.SendTransacEmail(context.TODO(), brevo.SendSmtpEmail{
+		Sender: &brevo.SendSmtpEmailSender{
+			Name:  os.Getenv("EMAIL_FROM_NAME"),
+			Email: os.Getenv("EMAIL_FROM_ADDRESS"),
+		},
+		To: []brevo.SendSmtpEmailTo{
+			{
+				Email: os.Getenv("EMAIL_TO_ADDRESS"),
+				Name:  os.Getenv("EMAIL_TO_NAME"),
+			},
+		},
+		Subject:     "Test Email from Brevo API",
+		TextContent: "This is a test email sent using the Brevo API.",
+	})
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.4
 require (
 	github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.32
 	github.com/brianvoe/gofakeit/v6 v6.28.0
+	github.com/getbrevo/brevo-go v1.1.3
 	github.com/go-co-op/gocron v1.37.0
 	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
@@ -14,7 +15,7 @@ require (
 )
 
 require (
-	github.com/getbrevo/brevo-go v1.1.3 // indirect
+	github.com/antihax/optional v1.0.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -26,6 +27,7 @@ require (
 	github.com/stretchr/testify v1.10.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
+	golang.org/x/oauth2 v0.21.0 // indirect
 	golang.org/x/sync v0.15.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
